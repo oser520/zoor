bea92e7be4d7d4ed2f6e23a7aade846852f695df
Omar Serrano <oser520@gmail.com>
2015-12-19 10:41:59 -0800

    Created PieceCount object to hold count of pieces

--- src/board.hh
+++ src/board.hh
@@ -7,6 +7,8 @@
 #ifndef _BOARD_H
 #define _BOARD_H
 
+#include <iostream>
+
 namespace zoor {
 
 /* TODO:
@@ -26,15 +28,50 @@ namespace zoor {
  *   - is mate
  */
 
+/**
+ * PieceCount maintains a count of the number of pieces on the board.
+ */
+struct PieceCount {
+  using count_type = unsigned short;
+
+  count_type mKings;
+  count_type mQueens;
+  count_type mRooks;
+  count_type mBishops;
+  count_type mKnights;
+  count_type mPawns;
+  count_type mTotal;
+
+  /* copy control */
+  PieceCount() noexcept = default;
+  PieceCount(const PieceCount &pcount) noexcept = default;
+  PieceCount& operator=(const PieceCount &pcount) noexcept = default;
+  ~PieceCount() noexcept = default;
+
+  /**
+   * clear() Clears the board of pieces.
+   * @return a reference to this PieceCount
+   */
+  PieceCount& clear() noexcept;
+
+  /**
+   * init() Sets the number of pieces to what they are when the game begins.
+   * @return a reference to this PieceCount
+   */
+  PieceCount& init() noexcept;
+};
+
 class Board {
+  friend std:ostream& operator<<(std:ostream &os, const Board &board);
+  unsigned int mRows[8];
 public:
-	/* copy control */
-	Board() = default;
-	Board(const Board &board) = default;
-	Board(Board &&board) = default;
-	Board& operator=(const Board &board) = default;
-	Board& operator=(Board &&board) = default;
-	~Board() = default;
+  /* copy control */
+  Board() = default;
+  Board(const Board &board) = default;
+  Board(Board &&board) = default;
+  Board& operator=(const Board &board) = default;
+  Board& operator=(Board &&board) = default;
+  ~Board() = default;
 };
 
 } // namespace zoor
