b858a9fc8372c3817c6ddb96f0c0f35716476024
Omar Serrano <oser520@gmail.com>
2015-12-25 22:26:46 -0800

    Basic strategy file info

---
+++ src/strategy.cc
@@ -0,0 +1,5 @@
+/**
+ * @file strategy.cc
+ * @author Omar A Serrano
+ * @date 2015-12-25
+ */
---
+++ src/strategy.hh
@@ -0,0 +1,9 @@
+/**
+ * @file strategy.hh
+ * @author Omar A Serrano
+ * @date 2015-12-25
+ */
+
+#ifndef _STRATEGY_H
+#define _STRATEGY_H
+#endif // _STRATEGY_H 
