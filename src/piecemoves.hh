5f00bb9e3eadbec6b857d8112e59fe9a8d426b68
Omar Serrano <oser520@gmail.com>
2015-12-29 20:31:43 -0800

    Class declaration for PieceMoves.

--- src/piecemoves.hh
+++ src/piecemoves.hh
@@ -3,9 +3,23 @@
  * @author Omar A Serrano
  * @date 2015-12-29
  */
-
 #ifndef _PIECEMOVES_H
 #define _PIECEMOVES_H
+
+namespace zoor {
+
+/**
+ * @brief Represents a list of moves for a given piece from a particular position in
+ * the board.
+ *
+ * @c PieceMoves is aware of the piece type, the location in the board, and whether
+ * a given move is legal.
+ */
+class PieceMoves
+{
+};
+
+} // namespace zoor
 
 #endif // _PIECEMOVES_H
 
