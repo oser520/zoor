bd82330f9759abced9bc00bce37d4ce97d281ad7
Omar Serrano <oser520@gmail.com>
2015-12-31 10:19:13 -0800

    Deleted class Square from header file.

--- src/piecemoves.hh
+++ src/piecemoves.hh
@@ -13,41 +13,6 @@
 namespace zoor {
 
 /**
- * Represents a position in the baord.
- */
-class Square
-{
-  friend std::ostream& operator<<(std::ostream&, const Square&);
-  friend bool operator==(const Square&, const Square&) noexcept;
-
-public:
-  using dim_type = unsigned short;
-  static constexpr dim_type BOARD_DIM = 8;
-  
-  // standard copy control
-  Square() noexcept = default
-  Square(const Square&) noexcept = default
-  Square(Square&&) noexcept = default
-  Square& operator=(const Square&) noexcept = default
-  Square& operator=(Square&&) noexcept = default
-  ~Square() noexcept = default;
-  // custom constructor
-  Square(dim_type, dim_type);
-
-  // getters
-  dim_type row() const noexcept;
-  dim_type col() const noexcept;
-
-  // setters
-  Square& setRow(dim_type) noexcept;
-  Square& setCol(dim_type) noexcept;
-
-private:
-  dim_type mRow;
-  dim_type mCol;
-};
-
-/**
  * @brief Represents a list of moves for a given piece from a particular position in
  * the board.
  *
