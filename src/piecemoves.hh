53e310643c567c3d1018a1576a252342ae8970b9
Omar Serrano <oser520@gmail.com>
2015-12-30 20:48:47 -0800

    Interface for Square.

--- src/piecemoves.hh
+++ src/piecemoves.hh
@@ -17,8 +17,34 @@ namespace zoor {
  */
 class Square
 {
-  unsigned short mRow;
-  unsigned short mCol;
+  friend std::ostream& operator<<(std::ostream&, const Square&);
+  friend bool operator==(const Square&, const Square&) noexcept;
+
+public:
+  using dim_type = unsigned short;
+  static constexpr dim_type BOARD_DIM = 8;
+  
+  // standard copy control
+  Square() noexcept = default
+  Square(const Square&) noexcept = default
+  Square(Square&&) noexcept = default
+  Square& operator=(const Square&) noexcept = default
+  Square& operator=(Square&&) noexcept = default
+  ~Square() noexcept = default;
+  // custom constructor
+  Square(dim_type, dim_type);
+
+  // getters
+  dim_type row() const noexcept;
+  dim_type col() const noexcept;
+
+  // setters
+  Square& setRow(dim_type) noexcept;
+  Square& setCol(dim_type) noexcept;
+
+private:
+  dim_type mRow;
+  dim_type mCol;
 };
 
 /**
