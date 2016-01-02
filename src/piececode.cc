7259312280a0c9983962576d3799615aad47be5e
Omar Serrano <oser520@gmail.com>
2016-01-01 18:26:00 -0800

    Implemented colorToString.

--- src/piececode.cc
+++ src/piececode.cc
@@ -61,6 +61,21 @@ std::string pieceToStringLong(const PieceCode &piece) noexcept
   }
 }
 
+std::string colorToString(const PieceColor &color) noexcept {
+  switch (color) {
+  case PieceColor::NONE:
+    return "N";
+  case PieceColor::WHITE:
+    return "W";
+  case PieceColor::BLACK:
+    return "B";
+  default:
+    // TODO: throw some kind of exception and make error message more informative by
+    // including function name, source file and line number
+    std::cerr << "Unknown PieceColor value" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
 std::string colorToStringLong(const PieceColor &color) noexcept {
   switch (color) {
   case PieceColor::NONE:
