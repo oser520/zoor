df8aa5c3c34da5edd5b7436b1c6395cc08b5c323
Omar Serrano <oser520@gmail.com>
2016-01-01 18:15:22 -0800

    Implemented pieceToString.

--- src/piececode.cc
+++ src/piececode.cc
@@ -6,7 +6,33 @@
 
 #include <string>
 
-#include "piececodes.hh"
+#include "piececode.hh"
 
 namespace zoor {
+
+std::string pieceToString(const PieceCode &piece) noexcept
+{
+  switch (piece) {
+  case PieceCode::NONE:
+    return "N";
+  case PieceCode::PAWN:
+    return "P";
+  case PieceCode::KNIGHT:
+    return "KN";
+  case PieceCode::BISHOP:
+    return "B";
+  case PieceCode::ROOK:
+    return "R";
+  case PieceCode::QUEEN:
+    return "Q";
+  case PieceCode::KING:
+    return "KI";
+  default:
+    // TODO: throw some kind of exception and make error message more informative by
+    // including function name, source file and line number
+    std::cerr << "Unknown PieceCode value" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+}
+
 } // namespace zoor
