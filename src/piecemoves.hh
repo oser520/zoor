4d599ca48e147351bd5e041f23bddaba6edd5efa
Omar Serrano <oser520@gmail.com>
2016-01-04 11:21:47 -0800

    Comment for operator!=.

--- src/piecemoves.hh
+++ src/piecemoves.hh
@@ -357,6 +357,14 @@ std::ostream& operator<<(std::ostream &os, const PieceMoves &pm);
  * @return True if pm1 and pm2 are equal, false otherwise.
  */
 bool operator==(const PieceMoves &pm1, const PieceMoves &pm2) noexcept;
+
+/**
+ * @brief Non-equality operator for @c PieceMoves.
+ * @detail The pieces making the moves and the moves made by each piece must be the same.
+ * @param pm1 The first @c PieceMoves.
+ * @param pm2 The second @c PieceMoves.
+ * @return True if pm1 and pm2 are different, false otherwise.
+ */
 bool operator=!(const PieceMoves &pm1, const PieceMoves &pm2) noexcept;
 
 } // namespace zoor
