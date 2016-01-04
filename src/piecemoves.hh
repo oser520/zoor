341f3672a4c6e1effd5bedeb6cdd7f2c5060d59a
Omar Serrano <oser520@gmail.com>
2016-01-03 21:03:33 -0800

    Comment also added to overlaod.

--- src/piecemoves.hh
+++ src/piecemoves.hh
@@ -198,6 +198,10 @@ public:
 
   /**
    * @brief Sets the @c Square of the piece making the moves.
+   *
+   * If this @c PieceMoves contains moves, then any move that is illegal from the new
+   * @c Square is removed.
+   *
    * @param square The square being moved.
    * @return A reference to this @c PieceMoves.
    * @throw Never throws exception.
