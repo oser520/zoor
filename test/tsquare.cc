c5af9dd8a4a9f02cfc5d9ee6cb9fed5c685114a8
Omar Serrano <oser520@gmail.com>
2016-01-06 19:52:57 -0800

    Boilerplate for unit tests.

--- test/tsquare.cc
+++ test/tsquare.cc
@@ -3,3 +3,15 @@
  * @author Omar A Serrano
  * @date 2016-01-05
  */
+
+#include "square.hh"
+#include "gtest/gtest.h"
+
+namespace {
+} // anonymous namespace
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
