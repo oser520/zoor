12720639773a1f311a492a6d1ab972f788b3978d
Omar Serrano <oser520@gmail.com>
2016-01-09 17:36:01 -0800

    No function valueKings().

--- test/tpiececount.cc
+++ test/tpiececount.cc
@@ -66,7 +66,6 @@ TEST(PieceCountTest, ValueFn)
   EXPECT_EQ(6, cp.valueBishops());
   EXPECT_EQ(10, cp.valueRooks());
   EXPECT_EQ(9, cp.valueQueens());
-  EXPECT_EQ(255, cp.valueKings());
 }
 
 TEST(PieceCountTest, OutputOp) {
