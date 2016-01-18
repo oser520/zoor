2fcc66d8c94e6254fc1e59523c3843dcc2af55f4
Omar Serrano <oser520@gmail.com>
2016-01-18 11:37:23 -0800

    Declared default move assignment.

--- src/board.hh
+++ src/board.hh
@@ -403,6 +403,13 @@ public:
    */
   BoardIterator(BoardIterator &&boardIterator) noexcept = default;
 
+  /**
+   * @brief Default move assignment.
+   * @param boardIterator The @c BoardIterator being moved.
+   * @throw Never throws.
+   */
+  BoardIterator& operator=(BoardIterator &&boardIterator) noexcept = default;
+
 private:
   /**
    * A pointer to the @c Board.
