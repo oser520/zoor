c5fc498acd9f9940a4b6c91cbca10ba71dd78bc0
Omar Serrano <oser520@gmail.com>
2016-01-29 23:26:42 -0800

    Defined operator&() for color and piece.

--- src/piececode.cc
+++ src/piececode.cc
@@ -86,4 +86,9 @@ std::ostream& operator<<(std::ostream &os, const PieceMask &mask)
   return os;
 }
 
+piececode_t operator&(const PieceColor color, const PieceCode piece) noexcept
+{
+  return static_cast<piececode_t>(color) & static_cast<piececode_t>(piece);
+}
+
 } // namespace zoor
