296f60c8588c071981923e745ffff42100079610
Omar Serrano <oser520@gmail.com>
2016-02-04 20:32:57 -0800

    Declared movePawn().

--- src/moves.hh
+++ src/moves.hh
@@ -13,4 +13,15 @@
 #include "board.hh"
 
 namespace zoor {
+
+/**
+ * @brief Move the pawn at the given row and column.
+ * @param row The row where the pawn is located.
+ * @param column The column where the pawn is located.
+ * @param board The board where the pawn will move.
+ * @return A vector of moves.
+ * @throw Never throws.
+ */
+vector<PieceMove>
+movePawn(dim_type row, dim_type column, const Board &board) noexcept;
 } // namespace zoor
