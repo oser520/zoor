5d908069675fa3e7d02b3c70558156b040595bcb
Omar Serrano <oser520@gmail.com>
2016-02-04 20:26:18 -0800

    Included headers and opened namespace.

--- src/moves.hh
+++ src/moves.hh
@@ -3,3 +3,14 @@
  * @author Omar A Serrano
  * @date 2016-02-03
  */
+
+// stl headers
+#include <vector>
+
+// zoor headers
+#include "piececode.hh"
+#include "piecemoves.hh"
+#include "board.hh"
+
+namespace zoor {
+} // namespace zoor
