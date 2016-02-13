22886d5cec54d8c0531e1079ca1c5358530f2d26
Omar Serrano <oser520@gmail.com>
2016-02-13 09:43:47 -0800

    Added minor comments.

--- src/piecemove.cc
+++ src/piecemove.cc
@@ -37,26 +37,31 @@ PieceMove::PieceMove
    dim_type toColumn) noexcept
   : mFrom(fromRow, fromColumn, code), mPromotion(toRow, toColumn) {}
 
+// from row getter
 dim_type PieceMove::fromRow() const noexcept
 {
   return mFrom.row();
 }
 
+// from column getter
 dim_type PieceMove::fromColumn() const noexcept
 {
   return mFrom.column();
 }
 
+// to row getter
 dim_type PieceMove::toRow() const noexcept
 {
   return mDestination.row();
 }
 
+// to column getter
 dim_type PieceMove::toColumn() const noexcept
 {
   return mDestination.column();
 }
 
+// source piece setter
 PieceMove& PieceMove::setPiece
   (dim_type row,
    dim_type column,
@@ -70,27 +75,27 @@ PieceMove& PieceMove::setPiece
   return *this;
 }
 
-PieceMove& PieceMove::setPiece
-  (dim_type row,
-   dim_type column,
-   piececode_t code) noexcept
+// source piece setter
+PieceMove&
+PieceMove::setPiece(dim_type row, dim_type column, piececode_t code) noexcept
 {
-  mFrom.setRow(row)
-       .setColumn(column)
-       .setPieceCode(piece);
+  mFrom.setRow(row).setColumn(column).setPieceCode(piece);
   return *this;
 }
 
+// source square getter
 Square PieceMove::fromSquare() const noexcept
 {
   return mFrom;
 }
 
-Square PieceMove::piece() const noexcept
+// source piece getter
+PieceCode PieceMove::piece() const noexcept
 {
   return mFrom.piece();
 }
 
+// capture setter
 PieceMove& PieceMove::setCapture
   (dim_type row,
    dim_type column,
@@ -104,27 +109,27 @@ PieceMove& PieceMove::setCapture
   return *this;
 }
 
-PieceMove& PieceMove::setCapture
-  (dim_type row,
-   dim_type column,
-   piececode_t code) noexcept
+// capture setter
+PieceMove&
+PieceMove::setCapture(dim_type row, dim_type column, piececode_t code) noexcept
 {
-  mCapture.setRow(row)
-          .setColumn(column)
-          .setPieceCode(piece);
+  mCapture.setRow(row).setColumn(column).setPieceCode(piece);
   return *this;
 }
 
+// does move result in a capture
 bool PieceMove::isCapture() const noexcept
 {
   return mCapture.piece() != PieceCode::NONE;
 }
 
+// capture getter
 Square PieceMove::capture() const noexcept
 {
   return mCapture;
 }
 
+// promotion setter
 PieceMove& PieceMove::setPromotion
   (dim_type row,
    dim_type column,
@@ -138,6 +143,7 @@ PieceMove& PieceMove::setPromotion
   return *this;
 }
 
+// promotion setter
 PieceMove& PieceMove::setPromotion
   (dim_type row,
    dim_type column,
@@ -149,6 +155,7 @@ PieceMove& PieceMove::setPromotion
   return *this;
 }
 
+// promotion setter
 PieceMove&
 PieceMove::setPromotion(PieceCode piece, PieceColor color) noexcept
 {
@@ -156,40 +163,47 @@ PieceMove::setPromotion(PieceCode piece, PieceColor color) noexcept
   return *this;
 }
 
+// promotion setter
 PieceMove& PieceMove::setPromotion(piececode_t code) noexcept
 {
   mPromotion.setPieceCode(code);
   return *this;
 }
 
+// does move result in promotion
 bool PieceMove::isPromotion() const noexcept
 {
   return mPromotion.piece() != PieceCode::NONE;
 }
 
+// promotion square
 Square PieceMove::promotion() const noexcept
 {
   return mPromotion;
 }
 
+// to row setter
 PieceMove& PieceMove::setToRow(dim_type row) noexcept
 {
   mPromotion.setRow(row);
   return *this;
 }
 
+// to column setter
 PieceMove& PieceMove::setToColumn(dim_type column) noexcept
 {
   mPromotion.setColumn(column);
   return *this;
 }
 
+// to row and to column setter
 PieceMove& PieceMove::setDestination(dim_type row, dim_type column) noexcept
 {
   mPromotion.setRow(row).setColumn(column);
   return *this;
 }
 
+// are moves equal
 bool operator==(const PieceMove &pm1, const PieceMove &pm2) noexcept
 {
   return pm1.mFrom == pm2.mFrom
@@ -197,11 +211,13 @@ bool operator==(const PieceMove &pm1, const PieceMove &pm2) noexcept
       && pm1.mCapture == pm2.mCapture;
 }
 
+// are moves different
 bool operator!=(const PieceMove &pm1, const PieceMove &pm2) noexcept
 {
   return !(pm1 == pm2);
 }
 
+// output stream
 std::ostream& operator<<(std::ostream &os, const PieceMove &pm)
 {
   os << "("
