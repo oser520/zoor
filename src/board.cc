5c369a7ae8c03f95bc483da31f92bd64f607f356
Omar Serrano <oser520@gmail.com>
2016-02-14 11:57:35 -0800

    Created stub function for isCheckRook().

--- src/board.cc
+++ src/board.cc
@@ -118,6 +118,12 @@ bool Board::isCheckBishop(dim_type row, dim_type column) const noexcept
   return false;
 }
 
+// TODO: implement
+bool Board::isCheckRook(dim_type row, dim_type column) const noexcept
+{
+  return false;
+}
+
 // is there a check from a rook
 bool Board::isCheckRook(dim_type row, dim_type column) const noexcept
 {
