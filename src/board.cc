e3b75cd715b6f39ea8b6097294e023f78240efa7
Omar Serrano <oser520@gmail.com>
2016-02-14 11:57:56 -0800

    Created stub function for isCheckQueen().

--- src/board.cc
+++ src/board.cc
@@ -124,6 +124,12 @@ bool Board::isCheckRook(dim_type row, dim_type column) const noexcept
   return false;
 }
 
+// TODO: implement
+bool Board::isCheckQueen(dim_type row, dim_type column) const noexcept
+{
+  return false;
+}
+
 // is there a check from a rook
 bool Board::isCheckRook(dim_type row, dim_type column) const noexcept
 {
