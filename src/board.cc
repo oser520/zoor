1b5da7ff3dbf6c5a7d73a9b4e6e28944678511ef
Omar Serrano <oser520@gmail.com>
2016-02-15 09:33:07 -0800

    Use functions in asserts.

--- src/board.cc
+++ src/board.cc
@@ -586,10 +586,9 @@ std::vector<PieceMove>
 Board::moveBishop(dim_type row, dim_type column) const noexcept
 {
   assert(mColor != PieceColor::NONE);
-  assert(row >= 0 && row < BOARD_DIM);
-  assert(column >= 0 && column < BOARD_DIM);
+  assert(isInBound(row, column));
   auto fromCode = get(row, column);
-  assert(getPieceCode(fromCode) == PieceCode::BISHOP);
+  assert(isBishop(fromCode));
   std::vector<PieceMove> moveList;
 
   // check all moves right and up
