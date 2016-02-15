3d7209660018877479d0025eb773397f63c3b1ec
Omar Serrano <oser520@gmail.com>
2016-02-15 09:34:43 -0800

    Use functions in asserts in movePawn.

--- src/board.cc
+++ src/board.cc
@@ -427,10 +427,9 @@ std::vector<PieceMove>
 Board::movePawn(dim_type row, dim_type column) const noexcept
 {
   assert(mColor != PieceColor::NONE);
-  assert(row >= 0 && row < BOARD_DIM);
-  assert(column >= 0 && column < BOARD_DIM);
+  assert(isInBound(row, column));
   auto fromCode = get(row, column);
-  assert(getPieceCode(fromCode) == PieceCode::PAWN);
+  assert(isPawn(fromCode));
 
   std::vector<PieceMove> moveList;
   dim_type cmpRow;
