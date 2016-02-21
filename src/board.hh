722c9898534b99b55b2a2e98593bcad1c4179c07
Omar Serrano <oser520@gmail.com>
2016-02-20 19:06:16 -0800

    Declared canCastle.

--- src/board.hh
+++ src/board.hh
@@ -122,6 +122,13 @@ private:
   unsigned short mBoardInfo;
 
   /**
+   * @brief Determine if the king whose turn is it to move can do short castling.
+   * @return True if the king whose turn can move can do short castling.
+   * @throw Never throws.
+   */
+  bool canCastle() const noexcept;
+
+  /**
    * @brief Determine if the king whose turn is it to move can do long castling.
    * @return True if the king whose turn can move can do long castling.
    * @throw Never throws.
