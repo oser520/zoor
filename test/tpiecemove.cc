5e1ed426257b29c81a2f1e8169ae72a7d27b9dda
Omar Serrano <oser520@gmail.com>
2016-02-21 10:07:29 -0800

    Added unit tests for long white castling.

--- test/tpiecemove.cc
+++ test/tpiecemove.cc
@@ -352,6 +352,36 @@ TEST(PieceMove16, shortCastling)
   EXPECT_FALSE(pm.isCastleLong());
 }
 
+// test long castling
+TEST(PieceMove17, longWhiteCastling)
+{
+  PieceMove pm;
+  EXPECT_FALSE(pm.isCastle());
+  EXPECT_FALSE(pm.isCastleLong());
+
+  pm.doCastleLong(PieceCode::WHITE);
+  // verify from squre
+  EXPECT_EQ(0, pm.fromRow());
+  EXPECT_EQ(4, pm.fromColumn());
+  EXPECT_EQ(PieceCode::KING, pm.fromPiece());
+  EXPECT_EQ(PieceColor::WHITE, pm.fromColor());
+
+  // verify destinatin square
+  EXPECT_EQ(0, pm.toRow());
+  EXPECT_EQ(2, pm.toColumn());
+  EXPECT_EQ(PieceCode::NONE, pm.promoPiece());
+  EXPECT_EQ(PieceColor::NONE, pm.promoColor());
+
+  // verify capture square
+  EXPECT_EQ(0, pm.captureRow());
+  EXPECT_EQ(0, pm.captureColumn());
+  EXPECT_EQ(PieceCode::NONE, pm.capturePiece());
+  EXPECT_EQ(PieceColor::NONE, pm.captureColor());
+
+  EXPECT_FALSE(pm.isCastle());
+  EXPECT_TRUE(pm.isCastleLong());
+}
+
 // test the equality operators
 TEST(PieceMove16, EqualOp) {
   auto code = PieceColor::BLACK | PieceCode::ROOK;
