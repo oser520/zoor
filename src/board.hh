380151b526a047b1c8ba9b56a3304ccc5bb46dbb
Omar Serrano <oser520@gmail.com>
2016-02-20 17:56:34 -0800

    Declared isCheckSW.

--- src/board.hh
+++ src/board.hh
@@ -156,6 +156,17 @@ private:
   bool isCheckNW(dim_type row, dim_type column, PieceCode piece) const noexcept;
 
   /**
+   * @brief Determine if there is a  check at the given row and column from a piece
+   *  in the diagonal down and to the right.
+   * @detail Can use this to determine if there is a check from a bishop or a queen.
+   * @param row The row where the king might be located.
+   * @param column The column where the king might be located.
+   * @return True if there is a check.
+   * @throw Never throws.
+   */
+  bool isCheckSW(dim_type row, dim_type column, PieceCode piece) const noexcept;
+
+  /**
    * @brief Determine if there is a pawn check at the given row and column.
    * @param row The row where the king is located.
    * @param column The column where the king is located.
