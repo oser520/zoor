f6a50091d412e577c6abc8f62e8ba5beeb5cb341
Omar Serrano <oser520@gmail.com>
2016-02-20 18:15:27 -0800

    Implemented isCheckE.

--- src/board.cc
+++ src/board.cc
@@ -218,6 +218,27 @@ Board::isCheckN(dim_type row, dim_type column, PieceCode piece) const noexcept
   return false;
 }
 
+// is it check in row from right
+bool
+Board::isCheckE(dim_type row, dim_type column, PieceCode piece) const noexcept
+{
+  assert(mColor != PieceColor::NONE);
+  assert(isInBound(row, column));
+
+  // check against rook above
+  for (auto toCol = row+1; toCol < BOARD_DIM; ++toCol) {
+    auto toCode = get(row, toCol);
+    if (isColorNone(toCode))
+      continue;
+    else if (isSameColor(toCode, mColor) || !isSamePiece(toCode, piece))
+      break;
+    else
+      return true;
+  }
+
+  return false;
+}
+
 // TODO: implement
 bool Board::isCheckPawn(dim_type row, dim_type column) const noexcept
 {
