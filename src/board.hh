e6357eeabad7062484a5c1fefafad0c8ed6dc223
Omar Serrano <oser520@gmail.com>
2016-02-21 08:43:00 -0800

    Removed two function declarations.

--- src/board.hh
+++ src/board.hh
@@ -133,20 +133,6 @@ private:
   bool canCastle() const noexcept;
 
   /**
-   * @brief Determine if the white king can do short castling.
-   * @return True if the white king can do short castling.
-   * @throw Never throws.
-   */
-  bool canWhiteCastle() const noexcept;
-
-  /**
-   * @brief Determine if the black king can do short castling.
-   * @return True if the black king can do short castling.
-   * @throw Never throws.
-   */
-  bool canBlackCastle() const noexcept;
-
-  /**
    * @brief Determine if the king whose turn is it to move can do long castling.
    * @return True if the king can do long castling.
    * @throw Never throws.
