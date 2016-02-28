f240fd824ff36f32280dbbf2a96820a0d148d69d
Omar Serrano <oser520@gmail.com>
2016-02-27 20:00:25 -0800

    Defined makeMoveCopy.

--- src/board.cc
+++ src/board.cc
@@ -1094,13 +1094,24 @@ std::vector<Board> Board::getBoards() const
   return boardList;
 }
 
-/**
- * TODO: implement makeMoveCopy function
- */
+// make a move on a copy of the board
 Board
 Board::makeMoveCopy(const PieceMove &pieceMove) const
 {
-  return Board();
+  auto sq = pieceMove.fromSquare();
+  auto fromCode = get(sq.row(), sq.column());
+
+  // verify correct piece in square
+  assert(isSamePiece(fromCode, sq.piece()) && isSameColor(fromCode, sq.color()));
+  auto moveList = getMoves(sq.row(), sq.column());
+  auto it = std::find(moveList.begin(), moveList.end(), pieceMove);
+
+  // verify move is legal
+  assert(it != moveList.end());
+  Board board(*this);
+  board.moveRef(pieceMove);
+
+  return board;
 }
 
 /**
