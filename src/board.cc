23309738b631650a1d7b29a2c9487d7276c9e787
Omar Serrano <oser520@gmail.com>
2016-03-01 19:45:34 -0800

    Redefined BoardIterator::operator*().

--- src/board.cc
+++ src/board.cc
@@ -1249,9 +1249,7 @@ BoardIterator::value_type BoardIterator::operator*() const
   if (mIndex >= LAST_INDEX)
     throw ChessError("Iterator cannot move beyond board");
 
-  auto row = mIndex >> 3;
-  auto col = mIndex & (Board::BOARD_DIM-1);
-  return mBoard.get(row, col);
+  return mCode;
 }
 
 // get the piece on the current square
