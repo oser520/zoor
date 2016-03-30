1169f31fdc96e6359e3ef4ea75bae145529178c4
Omar Serrano <oser520@gmail.com>
2016-03-29 20:46:03 -0700

    Added stub unit tests for BoardInfo.

--- test/tboardinfo.cc
+++ test/tboardinfo.cc
@@ -3,3 +3,100 @@
  * @author Omar A Serrano
  * @date 2016-03-29
  */
+
+#include <string>
+#include <functional>
+#include "boardinfo.hh"
+#include "gtest/gtest.h"
+
+namespace {
+
+using zoor::BoardInfo;
+
+// test get
+TEST(BoardInfo, DISABLED_get)
+{
+}
+
+// test rookA1 and rookA1On
+TEST(BoardInfo, DISABLED_rookA1)
+{
+}
+
+// test rookH1 and rookH1On
+TEST(BoardInfo, DISABLED_rookH1)
+{
+}
+
+// test wkMoved and wkMovedOn
+TEST(BoardInfo, DISABLED_wkMoved)
+{
+}
+
+// test wkCheck and wkCheckSet
+TEST(BoardInfo, DISABLED_wkCheck)
+{
+}
+
+// test wkMate and wkMateOn
+TEST(BoardInfo, DISABLED_wkMate)
+{
+}
+
+// test wkCastle and wkCastleLong
+TEST(BoardInfo, DISABLED_wkCastle)
+{
+}
+
+// test rookA8 and rookA8On
+TEST(BoardInfo, DISABLED_rookA8)
+{
+}
+
+// test rookH8 and rookH8On
+TEST(BoardInfo, DISABLED_rookH8)
+{
+}
+
+// test bkMoved and bkMovedOn
+TEST(BoardInfo, DISABLED_bkMoved)
+{
+}
+
+// test bkCheck and bkCheckSet
+TEST(BoardInfo, DISABLED_bkCheck)
+{
+}
+
+// test bkMate and bkMateOn
+TEST(BoardInfo, DISABLED_bkMate)
+{
+}
+
+// test bkCastle and bkCastleLong
+TEST(BoardInfo, DISABLED_bkCastle)
+{
+}
+
+// test toString and output stream
+TEST(BoardInfo, DISABLED_toString)
+{
+}
+
+// test hashCode and hash<BoardInfo>
+TEST(BoardInfo, DISABLED_hashCode)
+{
+}
+
+// test operator== and operator!=
+TEST(BoardInfo, DISABLED_operatorEq)
+{
+}
+
+} // anonymous namespace
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
