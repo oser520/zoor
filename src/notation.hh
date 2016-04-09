0ebdb642eaea21b21ef9447a0065ab15d8c9f441
Omar Serrano <oser520@gmail.com>
2016-04-08 21:52:25 -0700

    Add note about FEN records.

--- src/notation.hh
+++ src/notation.hh
@@ -11,6 +11,22 @@
 
 #include "board.hh"
 
+/**
+ * Example of FEN records.
+ *
+ * FEN record for starting position:
+ * rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
+ *
+ * After the move 1. e4:
+ * rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1
+ *
+ * After the move 1. .. c5:
+ * rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2
+ *
+ * After the move 2. Nf3:
+ * rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
+ */
+
 namespace zoor {
 
 /**
