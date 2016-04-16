732a4d2040f36cbc58667e3795f4ab9d1016f01e
Omar Serrano <oser520@gmail.com>
2016-04-16 16:39:39 -0700

    Put return type on separate line

--- src/piececount.hh
+++ src/piececount.hh
@@ -223,8 +223,8 @@ PieceCount::PieceCount() noexcept
 //
 // clear the piece count for each piece
 //
-inline
-PieceCount& PieceCount::clear() noexcept
+inline PieceCount&
+PieceCount::clear() noexcept
 {
   mWhite = mBlack = 0;
   return *this;
