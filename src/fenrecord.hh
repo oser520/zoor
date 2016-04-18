57d890ad0565159480ef9df044729fb8fec6b6c2
Omar Serrano <oser520@gmail.com>
2016-04-17 19:38:28 -0700

    Create header file for FenRecord
    Used to store the information for a single game found in a file that
    uses the Forsyth-Edwards notation.

---
+++ src/fenrecord.hh
@@ -0,0 +1,11 @@
+/// @file fenrecord.hh
+/// @author Omar A Serrano
+/// @date 2016-04-17
+/// @details Class declaration for a Forsyth-Edwards Notation (FEN) record.
+#ifndef _FENRECORD_H
+#define _FENRECORD_H
+
+namespace zoor {
+} // namespace zoor
+
+#endif // _FENRECORD_H
