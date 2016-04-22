ec44aee9a89410bf0d6adc3817441808fd7d978e
Omar Serrano <oser520@gmail.com>
2016-04-21 19:41:30 -0700

    Create unit test source file for iofen
    Start with basic test to read default board.

---
+++ test/tiofen.cc
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////////
+/// @file tiofen.cc
+/// @author Omar A Serrano
+/// @date 2016-04-21
+/////////////////////////////////////////////////////////////////////////////////////
+
+//
+// zoor headers
+//
+#include "board.hh"
+#include "fennotation.hh"
+
+//
+// gtest header
+//
+#include "gtest/gtest.h"
+
+namespace {
+
+//
+// using from zoor
+//
+using zoor::Board;
+using zoor::readFen;
+
+//
+// Test FenRecord
+//
+TEST(IOFen1, defaultBoard)
+{
+  Board board;
+  auto fenrec = readFen("test1.fen");
+
+  EXPECT_EQ(0, fr.halfMove());
+  EXPECT_EQ(0, fr.fullMove());
+  EXPECT_EQ(board, *fenrec.boardPtr());
+}
+
+} // namespace
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
