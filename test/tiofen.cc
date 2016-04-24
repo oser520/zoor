319317aa24b5c0c6659d94e03180d143c0f1015c
Omar Serrano <oser520@gmail.com>
2016-04-24 09:58:30 -0700

    Create test1 for readFen

--- test/tiofen.cc
+++ test/tiofen.cc
@@ -5,10 +5,18 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 //
+// STL
+//
+#include <vector>
+
+//
 // zoor headers
 //
 #include "board.hh"
+#include "boardinfo.hh"
 #include "fennotation.hh"
+#include "piececode.hh"
+#include "square.hh"
 
 //
 // gtest header
@@ -18,9 +26,18 @@
 namespace {
 
 //
+// using from STL
+//
+using std::vector;
+
+//
 // using from zoor
 //
 using zoor::Board;
+using zoor::BoardInfo;
+using zoor::PieceCode;
+using zoor::PieceColor;
+using zoor::Square;
 using zoor::readFen;
 
 //
@@ -36,6 +53,40 @@ TEST(IOFen1, defaultBoard)
   auto fenrec = fenList.front();
   EXPECT_EQ(0, fenrec.halfMove());
   EXPECT_EQ(1, fenrec.fullMove());
+  EXPECT_EQ(board, *fenrec.boardPtr());
+}
+
+//
+// Test reading a fen file that only contains one line
+//
+TEST(IOFen2, test1)
+{
+  vector<Square> sqList;
+
+  // add the black pieces
+  sqList.emplace_back(7, 4, PieceCode::KING, PieceColor::BLACK);
+  sqList.emplace_back(7, 7, PieceCode::ROOK, PieceColor::BLACK);
+  sqList.emplace_back(6, 4, PieceCode::PAWN, PieceColor::BLACK);
+  sqList.emplace_back(6, 6, PieceCode::PAWN, PieceColor::BLACK);
+
+  // add the white pieces
+  sqList.emplace_back(3, 2, PieceCode::KNIGHT, PieceColor::WHITE);
+  sqList.emplace_back(0, 3, PieceCode::QUEEN, PieceColor::WHITE);
+  sqList.emplace_back(0, 4, PieceCode::KING, PieceColor::WHITE);
+
+  // only black king can do short castling
+  BoardInfo info;
+  info.rookA1On().rookH1On().rookA8On();
+
+  Board board(sqList, PieceColor::WHITE, info);
+
+  auto fenList = readFen("fen/test1.fen");
+
+  EXPECT_EQ(1, fenList.size());
+
+  auto fenrec = fenList.front();
+  EXPECT_EQ(0, fenrec.halfMove());
+  EXPECT_EQ(33, fenrec.fullMove());
   EXPECT_EQ(board, *fenrec.boardPtr());
 }
 
