629beb824a154567d36d11a5f4930e818181a7e7
Omar Serrano <oser520@gmail.com>
2016-04-24 17:27:34 -0700

    Create unit test test2
    Reads a file that contains 19 FEN records.

--- test/tiofen.cc
+++ test/tiofen.cc
@@ -90,6 +90,130 @@ TEST(IOFen2, test1)
   EXPECT_EQ(board, *fenrec.boardPtr());
 }
 
+//
+// Test reading a fen file with 18 records
+//
+TEST(IOFen3, test2)
+{
+  // read the FEN records
+  auto fenList = readFen("fen/test2.fen");
+  EXPECT_EQ(19, fenList.size());
+
+  for (auto& fenrec : fenList) {
+    EXPECT_EQ(0, fenrec.halfMove());
+    EXPECT_EQ(40, fenrec.fullMove());
+  }
+
+  // default BoardInfo on all tests
+  BoardInfo info;
+
+  vector<Square> sqList;
+  // position in 1st fen line
+  // add the black pieces
+  sqList.emplace_back(6, 2,  PieceCode::KING, PieceColor::BLACK);
+  sqList.emplace_back(4, 1, PieceCode::PAWN, PieceColor::BLACK);
+  // add the white pieces
+  sqList.emplace_back(2, 4, PieceCode::KING, PieceColor::WHITE);
+  sqList.emplace_back(4, 5, PieceCode::PAWN, PieceColor::WHITE);
+
+  // compare againts 1st fen line
+  Board board1(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board1, *fenList[0].boardPtr());
+
+  // compare against 2nd fen line
+  sqList[0].setLocation(7, 1);
+  Board board2(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board2, *fenList[1].boardPtr());
+
+  // compare against 3rd fen line
+  sqList[0].setColumn(2);
+  Board board3(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board3, *fenList[2].boardPtr());
+
+  // compare against 4th fen line
+  sqList[0].setColumn(3);
+  Board board4(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board4, *fenList[3].boardPtr());
+
+  // compare against 5th fen line
+  sqList[0].setLocation(6, 1);
+  Board board5(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board5, *fenList[4].boardPtr());
+
+  // compare against 6th fen line
+  sqList[0].setColumn(3);
+  Board board6(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board6, *fenList[5].boardPtr());
+
+  // compare against 7th fen line
+  sqList[0].setLocation(5, 1);
+  Board board7(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board7, *fenList[6].boardPtr());
+
+  // compare against 8th fen line
+  sqList[0].setColumn(2);
+  Board board8(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board8, *fenList[7].boardPtr());
+
+  // compare against 9th fen line
+  sqList[0].setColumn(3);
+  Board board9(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board9, *fenList[8].boardPtr());
+
+  // compare against 10th fen line
+  sqList[0].setLocation(6, 2);
+  sqList[1].setLocation(3, 1);
+  Board board10(sqList, PieceColor::WHITE, info);
+  EXPECT_EQ(board10, *fenList[9].boardPtr());
+
+  // compare against 11th fen line
+  sqList[1].setRow(4);
+  sqList[2].setLocation(3, 3);
+  Board board11(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board11, *fenList[10].boardPtr());
+
+  // compare against 12th fen line
+  sqList[2].setColumn(4);
+  Board board12(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board12, *fenList[11].boardPtr());
+
+  // compare against 13th fen line
+  sqList[2].setColumn(5);
+  Board board13(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board13, *fenList[12].boardPtr());
+
+  // compare against 14th fen line
+  sqList[2].setLocation(2, 3);
+  Board board14(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board14, *fenList[13].boardPtr());
+
+  // compare against 15th fen line
+  sqList[2].setColumn(5);
+  Board board15(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board15, *fenList[14].boardPtr());
+
+  // compare against 16th fen line
+  sqList[2].setLocation(1, 3);
+  Board board16(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board16, *fenList[15].boardPtr());
+
+  // compare against 17th fen line
+  sqList[2].setColumn(4);
+  Board board17(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board17, *fenList[16].boardPtr());
+
+  // compare against 18th fen line
+  sqList[2].setColumn(5);
+  Board board18(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board18, *fenList[17].boardPtr());
+
+  // compare against 19th fen line
+  sqList[2].setLocation(2, 4);
+  sqList[3].setLocation(5, 5);
+  Board board19(sqList, PieceColor::BLACK, info);
+  EXPECT_EQ(board19, *fenList[18].boardPtr());
+}
+
 } // namespace
 
 int main(int argc, char *argv[])
