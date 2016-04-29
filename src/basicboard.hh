c7056841cddae6e3059cd81e94b47043d1468f95
Omar Serrano <oser520@gmail.com>
2016-04-28 20:32:27 -0700

    Create header file for BasicBoard.
    BasicBoard will be used to manage the dynamic array used to store the
    pieces. There are other alternatives, such as array and vector, but
    BasicBoard will be smaller than both, making move operations faster.

---
+++ src/basicboard.hh
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////////////////////////////////////
+/// @file basicboard.hh
+/// @author Omar A Serrano
+/// @date 2016-04-28
+/////////////////////////////////////////////////////////////////////////////////////
+#ifndef _BASICBOARD_H
+#define _BASICBOARD_H
+
+namespace zoor {
+
+///
+/// @brief A simple board that represents the position in a chess board.
+///
+class BasicBoard:
+{
+public:
+private:
+};
+
+} // zoor
+#endif // _BASICBOARD_H
