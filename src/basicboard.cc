cba61caabbb0cf1ac3eb4ce67dde86bea8276402
Omar Serrano <oser520@gmail.com>
2016-04-30 17:35:01 -0700

    Defined current BoardIter interface

--- src/basicboard.cc
+++ src/basicboard.cc
@@ -5,9 +5,106 @@
 /////////////////////////////////////////////////////////////////////////////////////
 
 //
+// STL
+//
+#include <cassert>
+
+//
 // zoor
 //
 #include "basicboard.hh"
 
 namespace zoor {
+
+/////////////////////////////////////////////////////////////////////////////////////
+// BoardIter
+/////////////////////////////////////////////////////////////////////////////////////
+
+//
+// Ctor with pointer param
+//
+BoardIter(piece_t *piece) noexcept
+  : mIter(piece)
+{
+  assert(mIter != nullptr);
+}
+
+//
+// prefix increment
+//
+BoardIter&
+operator++() noexcept
+{
+  assert(mIter != nullptr);
+  ++mIter;
+  return *this;
+}
+
+//
+// postfix increment
+//
+BoardIter
+operator++(int) noexcept
+{
+  assert(mIter != nullptr);
+  BoardIter iter(*this);
+  ++mIter;
+  return iter;
+}
+
+//
+// prefix decrement
+//
+BoardIter&
+operator--() noexcept
+{
+  assert(mIter != nullptr);
+  --mIter;
+  return *this;
+}
+
+//
+// postfix decrement
+//
+BoardIter
+operator--(int) noexcept
+{
+  assert(mIter != nullptr);
+  BoardIter iter(*this);
+  --mIter;
+  return iter;
+}
+
+//
+// dereference operator
+//
+const BoardIter::reference
+operator*() const noexcept
+{
+  assert(mIter != nullptr);
+  return *mIter;
+}
+
+//
+// addition operator
+//
+BoardIter
+operator+(const BoardIter &iter, BoardIter::difference_type value) noexcept
+{
+  BoardIter other(iter);
+  other += value;
+  return other;
+}
+
+//
+// subtraction operator
+//
+BoardIter
+operator-(const BoardIter &iter, BoardIter::difference_type value) noexcept
+{
+  BoardIter other(iter);
+  other -= value;
+  return other;
+}
+
 } // zoor
