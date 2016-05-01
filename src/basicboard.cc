ecb5ebf22447ffe35a4149edc5dd35e4869c4c29
Omar Serrano <oser520@gmail.com>
2016-04-30 18:05:38 -0700

    Defined equality operator for BoardIter

--- src/basicboard.cc
+++ src/basicboard.cc
@@ -127,4 +127,15 @@ operator-(const BoardIter &iter, BoardIter::difference_type value) noexcept
   return other;
 }
 
+//
+// equality operator
+//
+bool
+operator==(const BoardIter &iter1, const BoardIter &iter2) noexcept
+{
+  assert(iter1.mIter != nullptr);
+  assert(iter2.mIter != nullptr);
+  return iter1.mIter == iter2.mIter;
+}
+
 } // zoor
