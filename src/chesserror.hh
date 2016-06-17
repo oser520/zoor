0c466909f214d6be26ceee7b27ef588ee3506517
Omar Serrano <oser520@gmail.com>
2016-06-16 21:15:36 -0700

    Change doxygen sytle: /// -> //!

--- src/chesserror.hh
+++ src/chesserror.hh
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////////////////////////
-/// @file chesserror.hh
-/// @author Omar A Serrano
-/// @date 2016-02-28
+//! @file chesserror.hh
+//! @author Omar A Serrano
+//! @date 2016-02-28
 ////////////////////////////////////////////////////////////////////////////////
 #ifndef _CHESSERROR_H
 #define _CHESSERROR_H
@@ -14,29 +14,21 @@
 
 namespace zoor {
 
-///
-/// @brief A simple exception class to represent chess errors.
-///
+//! @brief A simple exception class to represent chess errors.
 class ChessError
   : public std::logic_error
 {
 public:
-  ///
-  /// @brief Default ctor.
-  ///
+  //! @brief Default ctor.
   ChessError(): std::logic_error("Bad chess logic") {}
 
-  ///
-  /// @brief Constructor.
-  /// @param msg The exception message.
-  ///
+  //! @brief Constructor.
+  //! @param msg The exception message.
   explicit
   ChessError(const char *msg): std::logic_error(msg) {}
 
-  ///
-  /// @brief Constructor.
-  /// @param msg The exception message.
-  ///
+  //! @brief Constructor.
+  //! @param msg The exception message.
   explicit
   ChessError(const std::string &msg): std::logic_error(msg) {}
 };
