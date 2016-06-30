492102422f25dbab6cfd59c49db115bc123b7028
Omar Serrano <oser520@gmail.com>
2016-06-29 19:41:00 -0700

    Stub definition of BasicStrategy

---
+++ src/basicstrategy.cc
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////
+//! @file basicstrategy.cc
+//! @author Omar A Serrano
+//! @date 2016-06-29
+////////////////////////////////////////////////////////////////////////////////
+
+//
+// zoor
+//
+#include "basicstrategy.hh"
+#include "board.hh"
+
+namespace zoor {
+
+int
+BasicStrategy::score(const Board &board) noexcept
+{
+  return 0;
+}
+
+} // namespace zoor
