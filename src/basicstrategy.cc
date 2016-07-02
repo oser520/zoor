d0b3e0e63daba1b9e7427158f6ee79d0356b5394
Omar Serrano <oser520@gmail.com>
2016-07-02 08:09:45 -0700

    Add list of factors that need to be evaluated

--- src/basicstrategy.cc
+++ src/basicstrategy.cc
@@ -15,6 +15,22 @@ namespace zoor {
 int
 BasicStrategy::score(const Board &board) noexcept
 {
+  // Factors to be evaluated
+  // 1. number of pieces for each color
+  // 2. how well are pieces positioned
+  //   - is king castled
+  //   - are knights out of the first rank
+  //   - are bishops out of the first rank
+  //   - are rooks connected
+  //   - are rooks in open column
+  //   - range of mobility of pieces, e.g., how many moves can a bishop make
+  //   - how many enemy pieces are being attacked
+  //   - how many pieces are in attacking position
+  //   - how many pieces are in danger
+  //   - is king in check
+  //   - are pawns connected
+  //   - are pawns advanced
+  //   - any doubled pawns
   return 0;
 }
 
