2785039100524e239627d015883563f523d38dbf
Omar Serrano <oser520@gmail.com>
2016-07-07 19:00:04 -0700

    Use doxygen style //!

--- src/search.hh
+++ src/search.hh
@@ -1,18 +1,13 @@
-/**
- * @file search.hh
- * @author Omar A Serrano
- * @date 2015-12-25
- */
- 
+////////////////////////////////////////////////////////////////////////////////
+//! @file search.hh
+//! @author Omar A Serrano
+//! @date 2015-12-25
+////////////////////////////////////////////////////////////////////////////////
 #ifndef _SEARCH_H
 #define _SEARCH_H
 
 namespace zoor {
-
-/**
- * TODO: Create search class that implements alpha-beta pruning. Each node will represent
- * a chess board position.
- */
+// TODO: Create search class that implements alpha-beta pruning. Each node will
+// represent a chess board position.
 }
-
 #endif // _SEARCH_H
