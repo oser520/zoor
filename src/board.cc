8db68ca9cb1af62b0c14fe56cc0d7c0a41cc617c
Omar Serrano <oser520@gmail.com>
2016-08-10 21:32:59 -0700

    Try new format for functions with long signatures

--- src/board.cc
+++ src/board.cc
@@ -59,12 +59,12 @@ Board::Board()
 //
 // constructor with list of pieces
 //
-Board::Board
-  (const std::vector<Square> &squareList,
-   const Color color,
-   const BoardInfo &boardInfo,
-   const PieceMove &lastMove)
-  : mBoard(BasicBoard::emptyBoard()),
+Board::Board(
+  const std::vector<Square> &squareList,
+  const Color color,
+  const BoardInfo &boardInfo,
+  const PieceMove &lastMove
+) : mBoard(BasicBoard::emptyBoard()),
     mColor(color),
     mLastMove(lastMove),
     mInfo(boardInfo)
